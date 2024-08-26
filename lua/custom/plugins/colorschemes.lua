14ac5c1f7d37c3969a1c438299be20c33cfb08ab
Allan Jarry <ajarry@aptean.com>
2024-08-26 14:09:55 +0200

    refactor: upt neotree gruvbox bg

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -8,7 +8,11 @@ return {
       -- directly inside the plugin declaration.
       -- vim.g.gruvbox_material_background = 'hard'
       -- vim.g.gruvbox_material_foreground = 'original'
+
       vim.cmd.colorscheme 'gruvbox-material'
+      vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = '#282828' })
+      vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = '#282828' })
+      vim.api.nvim_set_hl(0, 'NeoTreeEndOfBuffer', { bg = '#282828' })
     end,
   },
   'sainnhe/everforest',
