c65ebf4ca2933f8fc70906cab0e04383d8aecdba
Allan Jarry <ajarry@aptean.com>
2024-09-12 08:15:36 +0200

    refactor: switch colorscheme to everforest

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -9,13 +9,28 @@ return {
       -- vim.g.gruvbox_material_background = 'hard'
       -- vim.g.gruvbox_material_foreground = 'original'
 
-      vim.cmd.colorscheme 'gruvbox-material'
-      vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = '#282828' })
-      vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = '#282828' })
-      vim.api.nvim_set_hl(0, 'NeoTreeEndOfBuffer', { bg = '#282828' })
+      -- vim.cmd.colorscheme 'gruvbox-material'
+      -- vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = '#282828' })
+      -- vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = '#282828' })
+      -- vim.api.nvim_set_hl(0, 'NeoTreeEndOfBuffer', { bg = '#282828' })
     end,
   },
-  'sainnhe/everforest',
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      -- vim.g.gruvbox_material_background = 'hard'
+      -- vim.g.gruvbox_material_foreground = 'original'
+
+      vim.cmd.colorscheme 'everforest'
+      vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = '#2D353B' })
+      vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = '#2D353B' })
+      vim.api.nvim_set_hl(0, 'NeoTreeEndOfBuffer', { bg = '#2D353B' })
+    end,
+  },
   {
     'gbprod/nord.nvim',
     lazy = false,
